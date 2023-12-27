8c7e653a0d16cfb3f1196a0490b184ff2afbd24d
unknown <moaedadres22@gmail.com>
2023-12-27 18:54:11 +0300

    add pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
 const App = () => {
   return (
-    <div className='text-red-600'>App</div>
+    <Routes>
+      <Route path='/about' element={<About />} /> 
+      <Route path='/sign-in' element={<SignIn />} /> 
+      <Route path='/sign-out' element={<SignOut />} /> 
+      <Route path='/profile' element={<Profile />} /> 
+      <Route path='/' element={<Home />} /> 
+    </Routes>
   )
 }
 
