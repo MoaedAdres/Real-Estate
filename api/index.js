ce210ad5a072be7ea0a6f1a1ab627f40ffe524e4
unknown <moaedadres22@gmail.com>
2023-12-28 18:35:47 +0300

    make connection to mongo database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log("Mongo is Connected Successfuly"))
+.catch((error)=>{
+    console.log(error)
+})
 const app =express()
 
 app.listen(3000,()=>{
