e2e57854fa8b4b301d770a5aba6c273b84e0e2bc
unknown <moaedadres22@gmail.com>
2024-01-13 20:15:05 +0300

    create signup page

--- api/index.js
+++ api/index.js
@@ -5,20 +5,22 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.router.js'
 dotenv.config()
 mongoose.connect(process.env.MONGO)
-.then(()=>console.log("Mongo is Connected Successfuly"))
-.catch((error)=>{
-    console.log(error)
-})
-const app =express()
+    .then(() => console.log("Mongo is Connected Successfuly"))
+    .catch((error) => {
+        console.log(error)
+    })
+const app = express()
 
 app.use(express.json())
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Port is running on port 3000!!!")
 })
 
-app.use('/api/users',userRouter)
-app.use('/api/auth',authRouter)
+
+app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+
 app.use((error,req,res,next)=>{
     const statusCode=error.statusCode || 500
     const message=error.message|| "internal server error"
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Link } from 'react-router-dom'
+import * as Yup from 'yup';
+
+
+const SignUp = () => {
+  const SignupSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(5, 'minimum is 5')
+      .max(40, 'max is 40')
+      .required('Required'),
+    email: Yup.string().email("invalid email").required("email is required"),
+    password: Yup.string().required("password is required")
+  });
+  const initialValues = {
+    username: '',
+    email: '',
+    password: '',
+  }
+  const submitHandler = (values) => {
+    console.log(values)
+  }
+  return (
+    <div className='flex flex-col max-w-lg mx-auto'>
+
+      <div className='flex  flex-col gap-[10px]'>
+        <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+        <Formik
+          initialValues={initialValues}
+          enableReinitialize
+          validateOnChange
+          validationSchema={SignupSchema}
+          onSubmit={submitHandler}
+        >
+          {({ values, errors, touched, handleChange, handleSubmit }) => {
+            return (
+              <Form className='w-full gap-[25px] flex flex-col' onSubmit={handleSubmit}>
+                <div className='flex flex-col gap-[5px]'>
+                  <Field placeholder="username" className="border rounded-lg p-3" name="username" onChange={handleChange} value={values.username} />
+                  {errors.username && touched.username && <ErrorMessage name='username' />}
+                </div>
+                <div className='flex flex-col gap-[5px]'>
+                  <Field placeholder="email" className="border rounded-lg p-3" name="email" onChange={handleChange} value={values.email} />
+                  {errors.email && touched.email && <ErrorMessage name='email' />}
+                </div>
+                <div className='flex flex-col gap-[5px]'>
+                  <Field placeholder="password" className="border rounded-lg p-3" name="password" onChange={handleChange} value={values.password} />
+                  {errors.password && touched.password && <ErrorMessage name='password' />}
+                </div>
+                <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:bg-opacity-95 disabled:bg-opacity-80' type='submit'>
+                  SignUp
+                </button>
+              </Form>
+            )
+          }}
+        </Formik>
+      </div>
+      <div className='flex gap-2 mt-1'>
+        <p>Have an account</p>
+        <Link to="sign-in" >
+          <span className='text-blue-700 '>Sign in</span>
+        </Link>
+      </div>
+    </div>
+
+  )
+}
+export default SignUp
